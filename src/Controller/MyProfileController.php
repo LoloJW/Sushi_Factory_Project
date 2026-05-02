31f010ec5e35a13b0fd7b1a5d2afc5364096f0c6
LoloJW <laurentsaint-georges@live.fr>
2026-05-02 09:30:02 +0200

    Maj de l'interface my profile et fonctionnalité du bouton changer d'email

--- src/Controller/MyProfileController.php
+++ src/Controller/MyProfileController.php
@@ -21,51 +21,55 @@ final class MyProfileController extends AbstractController
         return $this->render('my_profile/index.html.twig');
     }
 
-#[Route('/profile/change-password', name: 'app_change_password', methods: ['GET', 'POST'])]
-public function changePasswordForm( Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
-{
-    $user = $this->getUser();
-    if (!$user instanceof User) {
-        throw $this->createAccessDeniedException();
-    }
-     $form = $this->createForm(ChangePasswordFormType::class, $user);
-     $form->handleRequest($request);
-
-     if ($form->isSubmitted() && $form->isValid()) {
-         $oldPassword = $form->get('oldPassword')->getData();
-        if (!$hasher->isPasswordValid($user,$oldPassword)) {
-            $this->addflash('error', 'Mot de passe entré incorrect.');
-            return $this->redirectToRoute('app_change_password');
+    #[Route('/profile/change-password', name: 'app_change_password', methods: ['GET', 'POST'])]
+    public function changePasswordForm( Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
         }
-        $user->setPassword($hasher->hashPassword($user, $form->get('password')->getData()));
-        $em->flush();
-        $this->addflash('success_password', 'Mot de passe modifié avec succès.');
-        return $this->redirectToRoute('app_my_profile');
-     }
-     return $this->render('my_profile/change_password.html.twig', [
-         'form' => $form->createView()
-     ]);
-}
+        $form = $this->createForm(ChangePasswordFormType::class, $user);
+        $form->handleRequest($request);
 
-#[Route('/profile/change-email', name: 'app_change_email', methods: ['POST', 'GET'])]
-public function changeEmail(Request $request, EntityManagerInterface $em, UserRepository $UR): Response
-{
-    $user = $this->getUser();
-    if (!$user instanceof User) {
-        throw $this->createAccessDeniedException();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $oldPassword = $form->get('oldPassword')->getData();
+            if (!$hasher->isPasswordValid($user,$oldPassword)) {
+                $this->addflash('error', 'Mot de passe entré incorrect.');
+                return $this->redirectToRoute('app_change_password');
+            }
+            $user->setPassword($hasher->hashPassword($user, $form->get('password')->getData()));
+            $em->flush();
+            $this->addflash('success_password', 'Mot de passe modifié avec succès.');
+            return $this->redirectToRoute('app_my_profile');
+        }
+        return $this->render('my_profile/change_password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
-    $newMail = $request->request->get('email');
-    $existingMail = $UR->findOneBy(['email' => $newMail]);
-    if ($existingMail) {
-        $this->addFlash('error_email', 'Cet email est deja utilisé.');
-        return $this->redirectToRoute('app_change_email');
+
+    #[Route('/profile/change-email', name: 'app_change_email', methods: ['POST', 'GET'])]
+    public function changeEmail(Request $request, EntityManagerInterface $em, UserRepository $UR): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        
+        if($request->isMethod("POST")){
+            $newMail = $request->request->get('email');
+            $existingMail = $UR->findOneBy(['email' => $newMail]);
+            if ($existingMail) {
+                $this->addFlash('error_email', 'Cet email est deja utilisé.');
+                return $this->redirectToRoute('app_change_email');
+            }
+            $user->setEmail($newMail);
+            $em->flush();
+    
+            $this->addFlash('success_email', 'Email modifié avec succès.');
+            return $this->redirectToRoute('app_my_profile');
+        }
+
+        return $this->render('my_profile/change_email.html.twig');
     }
-   
-
-    $user->setEmail($newMail);
-    $em->flush();
-
-    $this->addFlash('success_email', 'Email modifié avec succès.');
-    return $this->redirectToRoute('app_my_profile');
-}
 }
